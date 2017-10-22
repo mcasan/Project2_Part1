3c221c61056bb1f6383791d6a42f8ff04b595aee
mcasan <31909324+mcasan@users.noreply.github.com>
2017-10-22 18:19:56 -0400

    Add it

---
+++ Project2(Part1)/Assets/Scripts/Cube_Behavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_Behavior : MonoBehaviour {
+	
+public GameObject cubePrep;
+
+	void OnMouseDown () {
+
+
+		if (GameController.redcube != null) {
+
+			GameController.redcube.GetComponent<Renderer> ().material.color = Color.white;
+			}
+
+
+		gameObject.GetComponent<Renderer>().material.color = Color.red;
+
+		GameController.redcube = gameObject;
+
+
+	}
+}
---
+++ Project2(Part1)/Assets/Scripts/GameController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	
+	int Xposition;
+
+	public GameObject currentCube;
+
+	public GameObject cubePre;
+
+	public static GameObject redcube;
+
+	GameObject[]cubeRow = new GameObject[16];
+
+	Vector3 cubePosition;
+
+
+
+
+	// Use this for initialization
+
+	void Start () {
+
+		Xposition = -2;
+
+		cubePosition = new Vector3 (14, 0, 0);
+			
+		for (int i = 0; i < 16; i++){
+
+			cubeRow[i] = (GameObject) Instantiate (cubePre, cubePosition, Quaternion.identity);
+
+			cubePosition += new Vector3 (Xposition, 0, 0);
+
+			cubeRow[i].GetComponent<Renderer> ().material.color = Color.white;
+
+
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+		
+	}
+}
